15bff3ad2e23b72b5c08dec00a4575daf60ed209
Gabriel Carreño <carrenocarlosgabriel@gmail.com>
2022-06-29 20:35:29 -0300

    ...

--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,3 +1,15 @@
+var fs = require("fs");
+
+const queSoy = fs.readdir(".", function (err, files) {
+  if (err) throw err;
+  files.forEach(function (file) {
+    process.stdout.write(file.toString() + "\n");
+  });
+  process.stdout.write("prompt > ");
+});
+
+console.log("que soy:", queSoy);
+
 module.exports = {
   date: () => {
     process.stdout.write(Date());
