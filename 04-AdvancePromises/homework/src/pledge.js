72003b8eb9f2943403e821dfaad3994d1e372099
Gabriel Carreño <carrenocarlosgabriel@gmail.com>
2022-07-04 15:38:13 -0300

    second chapter finished

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -8,7 +8,9 @@ function $Promise(executor) {
   if (typeof executor !== "function") throw new TypeError("executor function");
   this._value = undefined;
   this._state = "pending";
-
+  this._handlerGroups = []; // [{sH,eH},{sH,eH},{sH,eH},{sH,eH}]...
+  //con el bind logro que todos los this con los que trabajen estos dos metodos, apunten a la instancia
+  //es como conectarle el this de la instancia $Promise a cada metodo...
   executor(this._internalResolve.bind(this), this._internalReject.bind(this));
 }
 
@@ -17,6 +19,7 @@ $Promise.prototype._internalResolve = function (someData) {
     // solo se cambia una sola vez el value y el state!!!!!
     this._state = "fulfilled";
     this._value = someData;
+    this.callHandlers();
   }
 };
 $Promise.prototype._internalReject = function (myReason) {
@@ -24,6 +27,36 @@ $Promise.prototype._internalReject = function (myReason) {
   if (this._state === "pending") {
     this._state = "rejected";
     this._value = myReason;
+    this.callHandlers();
+  }
+};
+
+$Promise.prototype.then = function (successCb, errorCb) {
+  // una forma:
+  /* if (typeof successCb !== "function" && typeof errorCb !== "function") {
+    this._handlerGroups.push(false);
+  } else {
+    this._handlerGroups.push({ successCb, errorCb });
+  } */
+  if (typeof successCb !== "function") successCb = false;
+  if (typeof errorCb !== "function") errorCb = false;
+  this._handlerGroups.push({ successCb, errorCb });
+  if (this._state !== "pending") {
+    this.callHandlers();
+  }
+};
+
+$Promise.prototype.callHandlers = function () {
+  while (this._handlerGroups.length > 0) {
+    // solo trabajare mientras tenga info en ese arr...
+    // [{SH1,EH1}, {SH2,EH2},{SH3,EH3}]
+    let current = this._handlerGroups.shift();
+    // ahora evaluo el state
+    if (this._state === "fulfilled") {
+      current.successCb && current.successCb(this._value);
+    } else if (this._state === "rejected") {
+      current.errorCb && current.errorCb(this._value);
+    }
   }
 };
 
