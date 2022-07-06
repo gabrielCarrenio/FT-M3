2d360c454536dd7311f0da363b18b839269e7016
Gabriel Carreño <carrenocarlosgabriel@gmail.com>
2022-07-06 11:34:00 -0300

    fin...

--- 05-Express/homework/src/server.js
+++ 05-Express/homework/src/server.js
@@ -11,10 +11,15 @@ let posts = []; // simula la db
 
 const server = express();
 // to enable parsing of json bodies for post requests
+//
+//params:
+// /posts/:id/:author/:title => /posts/hola/gabi/la historia de gabi
+// query: /posts/?id=hola&author=gabi&title=la historia de gabi //puedo obviar algunos de los parametros y seguiria funcionando! esa es la ventaja de query!
 
 server.use(express.json()); //aplico el midlware que me tradusca del japones al español para el server
 // TODO: your code to handle requests
 server.post("/posts", (req, res) => {
+  // normalmente trabajamos com body, en post y en put
   const { author, title, contents } = req.body;
   const error = {
     error: "No se recibieron los parámetros necesarios para crear el Post",
@@ -61,10 +66,18 @@ server.post("/posts/author/:author", (req, res) => {
 });
 
 server.get("/posts", (req, res) => {
-  const termParams = req.query;
+  //const termParams = req.query;
+  const { term } = req.query;
   //console.log(termParams);
   //logica de filtrar los post por term:
-  const postWithTerm = posts.filter((post) => {
+  if (term) {
+    const term_posts = posts.filter(
+      (p) => p.title.includes(term) || p.contents.includes(term)
+    );
+    return res.json(term_posts);
+  }
+  return res.json(posts); // por default envia un status 200
+  /*   const postWithTerm = posts.filter((post) => {
     if (
       post.title.split(" ").includes(termParams.term) &&
       post.contents.split(" ").includes(termParams.term)
@@ -80,7 +93,7 @@ server.get("/posts", (req, res) => {
     res.status(200).send(postWithTerm);
   } else {
     res.status(200).send(posts);
-  }
+  } */
 });
 
 server.get("/posts/:author", (req, res) => {
@@ -95,7 +108,8 @@ server.get("/posts/:author", (req, res) => {
       .status(STATUS_USER_ERROR)
       .send({ error: "No existe ningun post del autor indicado" });
   } else {
-    res.status(200).send(postsAuthorIndicado);
+    //res.status(200).send(postsAuthorIndicado);
+    res.json(postsAuthorIndicado);
   }
 });
 
@@ -111,80 +125,69 @@ server.get("/posts/:author/:title", (req, res) => {
       error: "No existe ningun post con dicho titulo y autor indicado",
     });
   } else {
-    res.status(200).send(postCoincidentes);
+    // res.status(200).send(postCoincidentes);
+    res.json(postCoincidentes);
   }
 });
 
 server.put("/posts", (req, res) => {
-  const paramsPost = req.body;
-  if (
-    paramsPost.hasOwnProperty("id") &&
-    paramsPost.hasOwnProperty("title") &&
-    paramsPost.hasOwnProperty("contents")
-  ) {
-    if (!posts.find(({ id }) => id === paramsPost.id)) {
-      res.status(STATUS_USER_ERROR).send({
-        error: "No se recibio el id necesario para modificar el Post",
-      });
+  const { id, title, contents } = req.body;
+  if (id && title && contents) {
+    //en el caso de estar trabajando por query o paramas, se reciben string entonces no es necesario parear a int!
+    let post = posts.find((p) => p.id === parseInt(id)); //esta la referencia del post!
+    if (post) {
+      // al trabajar con referencia, logro cambiar los datos:
+      post.title = title;
+      post.contents = contents;
+      postAct = post;
+      return res.json(post);
     } else {
-      let postAct;
-      posts.forEach((post) => {
-        // console.log("soy un post:", post);
-        if (post.id === paramsPost.id) {
-          post.title = paramsPost.title;
-          post.contents = paramsPost.contents;
-          postAct = post;
-        }
-      });
-      res.status(200).send(postAct);
+      res
+        .status(STATUS_USER_ERROR)
+        .json({ error: "No existe ningun post con dicho ID" });
     }
   } else {
-    res.status(STATUS_USER_ERROR).send({
-      error:
-        "No se recibieron los parámetros necesarios para modificar el Post",
+    res.status(STATUS_USER_ERROR).json({
+      error: "No se recibio el id necesario para modificar el Post",
     });
   }
 });
 
 server.delete("/posts", (req, res) => {
-  const body = req.body;
-  if (body.hasOwnProperty("id") && posts.find(({ id }) => id === body.id)) {
-    const nuevoPosts = [];
-    posts.forEach((post) => {
-      if (post.id !== body.id) {
-        nuevoPosts.push(post);
-      }
-    });
-
-    posts = nuevoPosts;
-    res.status(200).send({ success: true });
-  } else {
-    res.status(STATUS_USER_ERROR).send({ error: "Mensaje de error" });
+  const { id } = req.body;
+  const post = posts.find((p) => p.id === id);
+  if (!id || !post) {
+    return res.status(STATUS_USER_ERROR).json({ error: "Mensaje de error" });
   }
+  posts = posts.filter((p) => p.id !== parseInt(id)); //quedate con todos los elementos que no tengan ese id
+
+  res.json({ success: true });
 });
 
 server.delete("/author", (req, res) => {
-  const body = req.body;
-  if (
-    body.hasOwnProperty("author") &&
-    posts.find(({ author }) => author === body.author)
-  ) {
-    const postEliminados = [];
-    const nuevoPosts = [];
-    posts.forEach((post) => {
-      if (post.author === body.author) {
-        postEliminados.push(post);
-      } else {
-        nuevoPosts.push(post);
-      }
-    });
-
-    posts = nuevoPosts;
-    res.status(200).send(postEliminados);
-  } else {
-    res
+  const { author } = req.body;
+  const author_found = posts.find((p) => p.author === author);
+  if (!author || !author_found) {
+    return res
       .status(STATUS_USER_ERROR)
-      .send({ error: "No existe el autor indicado" });
+      .json({ error: "No existe el autor indicado" });
   }
+  const autoresEliminados = [];
+
+  posts = posts.filter((post) => {
+    if (post.author !== author) {
+      return true;
+    } else {
+      autoresEliminados.push(post);
+      // no  hace falta poner el return false, ya que filter no le interesa los false!
+    }
+    /*  if (post.author === author) {
+      autoresEliminados.push(post);
+    } else {
+      return post;
+    } */
+  });
+
+  res.json(autoresEliminados);
 });
 module.exports = { posts, server };
